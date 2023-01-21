80e666e3343bd50f3eea3c73b86946f9adc0c217
ekchew <ekchew@gmail.com>
2023-01-21 15:18:40 -0500

    a much-improved one-liner solution

--- makearray.hpp
+++ makearray.hpp
@@ -6,26 +6,11 @@
 #include <array>
 #include <utility>
 
-namespace details {
-    // Each arg passed to MakeArray() gets wrapped in a struct with an
-    // overloaded << operator that assigns it to a std::array iterator.
-    // This allows MakeArray() to build a C++17 fold expression around <<.
-    template<typename T> struct MakeArrayArg{ T&& v; };
-    template<typename It, typename T> constexpr
-        auto operator << (It it, MakeArrayArg<T> arg) -> It {
-            *it = std::forward<T>(arg.v);
-            return ++it;
-        }
-}
-
 /*---- MakeArray function template --------------------------------------------
  *
  *  MakeArray returns a std::array of explicit type T and inferred size.
  *  It takes a parameter pack of arguments that must be convertible to T, and
- *  the array size is taken from the number of arguments. The array is
- *  initially default-constructed before the arguments get copy/move-assigned
- *  to their corresponding array elements, meaning T must be
- *  default-constructible.
+ *  the array size is taken from the number of arguments.
  *
  *  Example:
  *
@@ -35,19 +20,13 @@ namespace details {
  */
 
 #if CONCEPTS_AVAILABLE
-    template<std::default_initializable T, std::convertible_to<T>... Args>
+    template<typename T, std::convertible_to<T>... Args>
 #else
     template<typename T, typename... Args>
 #endif
-    constexpr auto MakeArray(Args&&... args) {
-        static_assert(
-            __cplusplus > 201402L, "MakeArray() requires C++17 or later"
-            );
-        std::array<T, sizeof...(Args)> arr;
-        auto it = arr.begin();
-        (it << ... << details::MakeArrayArg<T>{std::forward<T>(args)});
-        return arr;
+    constexpr auto MakeArray(Args&&... args)
+    {
+        return std::array<T, sizeof...(Args)>{std::forward<T>(args)...};
     }
-
 
 #endif
